9da18725dcafaf36a65b5204d8d043b48350c6d7
Abdulmunaim Mahfudh <113377393+Abdulmunimsaleh@users.noreply.github.com>
2025-03-24 10:45:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import nltk
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
+nltk.download('punkt_tab')
 from nltk.stem import WordNetLemmatizer
 nltk.download('popular', quiet=True)   
 import requests
